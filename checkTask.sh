d059b0d6863569766962af5c888cb546ff09fb03
sinpu <sinpu11080@gmail.com>
2015-07-07 18:40:01 +0900

    add take fileName

--- checkTask.sh
+++ checkTask.sh
@@ -1,7 +1,20 @@
 
-files="./testData/*"
+files="./testData/*.c"
 for fp in $files; do
 	echo $fp
+	file=${fp##*/}
+	# take filename.*
+	echo $file
+
+	filename=${file%.*}
+	# take filename
+	echo $filename
+
+	#param=${filename##*.}
+	# take extension
+	#echo $param
+
+	#gcc 
 done
 
 
